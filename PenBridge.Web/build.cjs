f7a407671ee89617ed1f7fb27e608d5a715fc918
Tharu_81 <tharuminecraft6571@gmail.com>
2026-09-06 20:12:16 +0900

    Make web bundle builds deterministic

--- PenBridge.Web/build.cjs
+++ PenBridge.Web/build.cjs
@@ -1,10 +1,12 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const root = __dirname;
-const html = fs.readFileSync(path.join(root, 'pad.html'), 'utf8');
+const normalizeLineEndings = value => value.replace(/\r\n?/g, '\n');
+const html = normalizeLineEndings(fs.readFileSync(path.join(root, 'pad.html'), 'utf8'));
 fs.mkdirSync(path.join(root, 'dist'), { recursive: true });
 fs.writeFileSync(path.join(root, 'dist', 'pad-loader.js'),
   `document.open();document.write(${JSON.stringify(html)});document.close();\n`);
-const landing = fs.readFileSync(path.join(root, 'index.html'), 'utf8').replace("new URL('./dist/',location.href)", "new URL('./',location.href)");
+const landing = normalizeLineEndings(fs.readFileSync(path.join(root, 'index.html'), 'utf8'))
+  .replace("new URL('./dist/',location.href)", "new URL('./',location.href)");
 fs.writeFileSync(path.join(root, 'dist', 'index.html'), landing);
 console.log('Built PenBridge.Web/dist');
